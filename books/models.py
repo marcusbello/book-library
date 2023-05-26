42709777b0d24fd62ba188535afeef199a31d536
Marcus Bello <marcusthetechguru@gmail.com>
2023-05-26 10:55:04 -0400

    Book Lister Service

--- books/models.py
+++ books/models.py
@@ -4,7 +4,7 @@ from django.db import models
 
 class Book(models.Model):
     title = models.CharField(max_length=250)
-    substitle = models.CharField(max_length=250)
+    subtitle = models.CharField(max_length=250)
     author = models.CharField(max_length=100)
     isbn = models.CharField(max_length=13)
     
